3b3607a737a997ca201f0e43283e2729501679d4
banushi-a <altonbanushi@icloud.com>
2023-07-16 23:06:59 -0500

    Setup gsap

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -1,6 +1,8 @@
 import floodit from "../images/floodit-screenshot.png";
 import maze from "../images/maze.png";
 import ipa from "../images/IPA.png";
+import { useLayoutEffect, useRef } from "react";
+import { gsap } from "gsap";
 
 const projectObjects = [
   {
@@ -30,6 +32,12 @@ const projectObjects = [
 ];
 
 const Projects = (): JSX.Element => {
+  const titleRef = useRef<any>();
+
+  useLayoutEffect(() => {
+    gsap.fromTo(titleRef.current, { y: -100 }, { duration: 1, y: 0 });
+  }, []);
+
   return (
     <div
       className="text-white bg-my-blue"
@@ -38,7 +46,10 @@ const Projects = (): JSX.Element => {
           "linear-gradient(190deg, rgb(28, 28, 65) 30%, rgb(7, 152, 249) 100%)",
       }}
     >
-      <div className="container mx-auto flex pt-20 items-center justify-center">
+      <div
+        className="container mx-auto flex pt-20 items-center justify-center"
+        ref={titleRef}
+      >
         <h2 className="text-[4rem] md:text-[5rem] lg:text-[8rem] bg-gradient-to-t from-my-pink to-my-blue bg-clip-text text-transparent text-center">
           My Projects
         </h2>
