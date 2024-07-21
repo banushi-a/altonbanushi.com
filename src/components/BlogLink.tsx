a87f41b7e449dc419696770f0cacd0b74d50c449
banushi-a <altonbanushi@icloud.com>
2024-07-20 22:11:50 -0400

    Blog Page First Post

--- src/components/BlogLink.tsx
+++ src/components/BlogLink.tsx
@@ -1,13 +1,41 @@
 type BlogLinkType = {
   href: string;
+  title: string;
+  previewText: string;
+  image: string;
+  className?: string;
   target?: "_blank" | "_self" | "_parent" | "_top";
 };
 
-const BlogLink = ({ href, target = "_blank" }: BlogLinkType): JSX.Element => {
+const BlogLink = ({
+  href,
+  title,
+  previewText,
+  image,
+  className,
+  target = "_blank",
+}: BlogLinkType): JSX.Element => {
   return (
-    <a href={href} target={target} className="border border-red-500">
-      content
-    </a>
+    <div>
+      <div className="h-4 w-4 bg-white rounded-full absolute ml-[-40px] mt-6" />
+      <a
+        href={href}
+        target={target}
+        className={`grid grid-cols-7 hover:outline hover:outline-white hover:rounded-2xl p-4 gap-4 ${className}`}
+      >
+        <div className="col-span-7 md:col-span-5 pr-2 md:pr-4">
+          <h2 className="font-bold text-xl md:text-2xl lg:text-3xl">{title}</h2>
+          <p className="text-md italic md:text-lg lg:text-2xl">
+            {previewText}...
+          </p>
+        </div>
+        <img
+          src={image}
+          alt="statistics book page"
+          className="col-span-7 md:col-span-2 rounded-2xl block"
+        />
+      </a>
+    </div>
   );
 };
 
--- src/pages/Blog.tsx
+++ src/pages/Blog.tsx
@@ -1,78 +1,43 @@
-import { useLayoutEffect, useRef } from "react";
 import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 
+import BlogLink from "../components/BlogLink";
 import Logo from "../images/logo.png";
-import { Link } from "react-router-dom";
+import bookImage from "../images/blogImages/statsImage.png";
 
 gsap.registerPlugin(ScrollTrigger);
 
 const Blog = (): JSX.Element => {
-  const blogLinkRefs = useRef<any[]>([]);
-
   const blogLinks: JSX.Element[] = [
-    <h2
-      className="font-bold text-3xl text-white"
-      ref={(elem) => blogLinkRefs.current.push(elem)}
-    >
-      🚧 Page Under Construction 🚧
-    </h2>,
-    <h3
-      className="font-bold text-2xl text-white hover:underline"
-      ref={(elem) => blogLinkRefs.current.push(elem)}
-    >
-      <Link to="/">Back Home</Link>
-    </h3>,
+    <BlogLink
+      href="https://medium.com/@altonbanushi/what-are-you-going-to-do-with-math-18df2ff067cd"
+      title="What are you going to do with math?"
+      previewText="I’ve heard this question so, so many times. Be it from friends, family, or people I just introduced"
+      image={bookImage}
+    />,
   ];
-
-  // gsap animations
-  useLayoutEffect(() => {
-    blogLinkRefs.current.forEach((ref, i) => {
-      gsap.fromTo(
-        ref,
-        {
-          opacity: 0,
-          y: -50,
-        },
-        {
-          scrollTrigger: {
-            trigger: ref,
-            toggleActions: "restart none none none",
-            start: "50px 80%",
-            end: "50px 60%",
-            once: true,
-          },
-          opacity: 1,
-          y: 0,
-          duration: 2,
-          delay: i / 2,
-        }
-      );
-    });
-  }, []);
 
   return (
     <div
-      className="grid grid-cols-5"
+      className="grid grid-cols-7"
       style={{
         background:
           "linear-gradient(190deg, rgb(28, 28, 65) 30%, rgb(7, 152, 249) 100%)",
       }}
     >
-      <div className="flex flex-col p-8 col-span-5 md:col-span-1 text-white">
-        <div className="flex items-center h-14 gap-4">
-          <img src={Logo} alt="Logo" className="h-12" />
+      <div className="flex flex-col p-8 col-span-7 md:col-span-2 text-white border-b md:border-b-0 md:border-r border-white">
+        <div className="flex justify-end items-center h-14 gap-4">
           <h2 className="text-4xl font-bold bg-gradient-to-r from-my-pink to-my-blue bg-clip-text text-transparent">
             AB
           </h2>
+          <img src={Logo} alt="Logo" className="h-12" />
         </div>
-
-        <h2 className="font-semibold italic">
-          Exploring the place where comptuer science, mathematics, and
-          curiousity live
+        <h2 className="font-semibold italic text-right">
+          Exploring the place where computer science, mathematics, and curiosity
+          live
         </h2>
       </div>
-      <div className="text-white min-h-[95dvh] p-8 flex flex-col justify-start items-center gap-4 col-span-5 md:col-span-4">
+      <div className="text-white min-h-[95dvh] p-8 flex flex-col justify-start items-center gap-4 col-span-7 md:col-span-5">
         {blogLinks}
       </div>
     </div>
